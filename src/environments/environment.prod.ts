a1fc9154cef7c249fae9fa2380362a1b3879d3c0
navneelkuppili <navneelkuppili@gmail.com>
2018-06-25 10:00:40 -0400

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBqFILYUFbB9d3zOizOnJbpeVJXr8072FA",
+    authDomain: "ecommerce-app-70e62.firebaseapp.com",
+    databaseURL: "https://ecommerce-app-70e62.firebaseio.com",
+    projectId: "ecommerce-app-70e62",
+    storageBucket: "ecommerce-app-70e62.appspot.com",
+    messagingSenderId: "342921231130"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBqFILYUFbB9d3zOizOnJbpeVJXr8072FA",
+    authDomain: "ecommerce-app-70e62.firebaseapp.com",
+    databaseURL: "https://ecommerce-app-70e62.firebaseio.com",
+    projectId: "ecommerce-app-70e62",
+    storageBucket: "ecommerce-app-70e62.appspot.com",
+    messagingSenderId: "342921231130"
+  }
 };
 
 /*
